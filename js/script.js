060574deaf8ddcebe8cad390a11edaaf32ae07fe
robertvlad <vladrob96@gmail.com>
2022-12-27 23:15:11 +0100

    Aggiunta lista To Do List

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
-new Vue({
-    el: '#app',
-    data: {
+const { createApp } = Vue;
 
-
+createApp({
+  data() {
+    return {
+        refTask: 0,
+        newTask: "",
+        taskList: [
+          { text: "Ripulire la stanza", done: false },
+          { text: "Fare la doccia", done: false },
+          { text: "Fare colazione", done: false },
+          { text: "Preparare la postazione", done: false },
+          { text: "Inizare a lavorare", done: false },
+        ],
+      };
     },
     methods: {
-
-
-    }
-});
+        addTask() {
+            let newObject = {
+              text: this.newTask,
+              done: false,
+            };
+      
+            this.taskList.push(newObject);
+            this.newTask = "";
+          },
+          delTask(index) {
+            this.taskList.splice(index, 1);
+        },
+    },
+}).mount("#appToDoList");
